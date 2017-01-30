587b8f8a80c1c8706d1d94cf5dc2c76dc39a3657
Silvina <stenconi@gmail.com>
2017-01-30 14:44:24 -0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Medref::Application.config.secret_key_base = '3a28f8de28b5785e807864340d3ff6f18c9769956587a6209c5937a97f39d70c40594a09dc7fab77178a0c4cc8816cb46dfbb213d351ae25d66d3da3d65e5e28'
